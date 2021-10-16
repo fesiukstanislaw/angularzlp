3c4c0e6541942aad1daa5cdbd313db1fb36c4d35
Stanislav Fesiuk <fesiuk.stanislaw@gmail.com>
2021-10-17 00:49:34 +0300

    added test component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {CommonModule} from "@angular/common";
+import { TestComponent } from './test/test.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
